664e8094182ba683b0dccba65a2de1416b030913
Ketanaa <86412087+Ketanaa@users.noreply.github.com>
2021-07-08 18:05:00 +0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
-var sea
-var ship
+var sea,seaIMG;
+var ship,shipIMG;
 
 
 function preload()
 {
   seaIMG = loadImage("sea.png");
-  ship = loadAnimation(ship-1.png,ship-2.png,ship-3.png,ship-4.png);
+  shipIMG = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
 
   sea.addImage(seaIMG);
 }
@@ -14,21 +14,24 @@ function setup()
 {
   createCanvas(400,400);
  
-  ship=createSprite();
   sea=createSprite(200,180)
+  sea.addImage(seaIMG);
+  sea.scale=0.5
+
+  ship=createSprite(200,200);
+  ship.addAnimation("moving ship",shipIMG);
+  ship.scale=0.25
 
 }
 
 function draw() {
-  background("blue");
- 
-  background.addImage(sea);
+  background("0");
 
   sea.velocityX=-2;
 
   if(sea.x < 0)
   {
-    sea.x = sea.width/2;
+    sea.x = sea.width/8;
   }
   
   drawSprites();
